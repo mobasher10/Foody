960199d3ced8fca11a76c51e56d3a18ed97c2af9
mobasher10 <73739199+mobasher10@users.noreply.github.com>
2022-10-21 11:26:17 +0300

    responsive

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -2,8 +2,25 @@
 
 const hamburger = document.querySelector(".hamburger__menu");
 const header = document.querySelector(".header");
+const heroSection = document.querySelector(".section__hero");
 
 // * MOBILE NAV
 hamburger.addEventListener("click", () => {
   header.classList.toggle("nav__open");
 });
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-70px",
+});
+
+headerObserver.observe(heroSection);
